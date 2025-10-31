acff45a34dfdc9a302ab5708ee20f6d860ce3abf
santosh kumar <vamsidola9@gmail.com>
2025-10-31 11:05:54 +0530

    Add files via upload

---
+++ palindrome checker.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int n, reversed = 0, remainder, original;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    original = n;
+    while (n != 0) {
+        remainder = n % 10;
+        reversed = reversed * 10 + remainder;
+        n /= 10;
+    }
+    if (original == reversed)
+        printf("%d is a palindrome.\n", original);
+    else;
+        printf("%d is not a palindrome.\n", original);
+}
